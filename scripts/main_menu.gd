64e5331887f769cac1206feb6a9bb97377223105
vcaut <vcautinepifani@greentube.com>
2025-01-25 20:17:56 +0100

    changes for introduction

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
-
+func _hide_all() -> void:
+	$CanvasLayer/StartSlide.visible = false
+	$CanvasLayer/Slide1.visible = false
+	$CanvasLayer/Slide2.visible = false
+	
 func _on_button_pressed() -> void:
+	_hide_all()
+	$CanvasLayer/Slide1.visible = true
+	
+
+
+func _on_slide_1_pressed() -> void:
+	_hide_all()
+	$CanvasLayer/Slide2.visible = true
+
+
+func _on_button_Slide2_pressed() -> void:
+	_hide_all()
 	GameManager.loadMainScene()
